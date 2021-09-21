3f31effc85de629ce00849f0b955871240b3d6c7
LuisSid55 <luiscitty4@gmail.com>
2021-09-21 20:07:28 -0300

    Adicionei comando para aparecer um desenho em texto

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace AsciiArt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Pressione uma tecla...");
+            Console.ReadKey();
+            Console.WriteLine();
+            Console.WriteLine(" ||  || ");
+            Console.WriteLine(" \\\\()// ");
+            Console.WriteLine("//(__)\\\\");
+            Console.WriteLine("||    ||");
+            Console.WriteLine();
+            Console.WriteLine("Pressione uma tecla...");
+            Console.ReadKey();
+            Console.WriteLine();
+            Console.WriteLine("=^_^=");
+            Console.WriteLine();
+        }
+    }
+}
