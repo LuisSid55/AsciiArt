98ef5010fb0155dfacfe8c42989ba6a8e11bffcb
LuisSid55 <luiscitty4@gmail.com>
2021-09-22 22:42:07 -0300

    Adicionei um carro

--- Program.cs
+++ Program.cs
@@ -20,6 +20,28 @@ namespace AsciiArt
             Console.WriteLine();
             Console.WriteLine("=^_^=");
             Console.WriteLine();
+            Console.WriteLine("Pressione uma tecla...");
+            Console.ReadKey();
+            Console.WriteLine();
+            Console.WriteLine(@"                   ___..................____");
+            Console.WriteLine(@"           _..--''~_______   _____...----~~~\\");
+            Console.WriteLine(@"       __.'    .-'~       \\~      [_`.7     \\");
+            Console.WriteLine(@" .---~~      .'            \\           __..--\\_");
+            Console.WriteLine(@"/             `-._          \\   _...~~~_..---~  ~~~~~~~~~~~~--.._");
+            Console.WriteLine(@"\              /  ~~~~~~----_\`-~_-~~__          ~~~~~~~---.._    ~--.__");
+            Console.WriteLine(" \\     _       |==            |   ~--___--------...__          `-   _.--''']");
+            Console.WriteLine(@"  \ __/.-._\   |              |            ~~~~--.  `-._ ___...--~~~_.'|_Y |");
+            Console.WriteLine(@"   `--'|/~_\\  |              |     _____           _.~~~__..--~~_.-~~~.-~/");
+            Console.WriteLine(@"     | ||| |\\_|__            |.../.----.._.        | Y |__...--~~_.-~  _/");
+            Console.WriteLine(@"      ~\\\ || ~|..__---____   |||||  .'~-. \\       |_..-----~~~~   _.~~");
+            Console.WriteLine(@"        \`-'/ /     ~~~----...|'''|  |/'_'\\ \\   |~~'           __.~");
+            Console.WriteLine(@"         `~~~'                 ~~-:  ||| ~| |\\  |        __..~~");
+            Console.WriteLine(@"                                   ~~|||  | | \\/  _.---~~");
+            Console.WriteLine(@"                                     \\\  //  | ~~~");
+            Console.WriteLine(@"                                      \`-'/  / dp");
+            Console.WriteLine(@"                                       `~~~~'");                                                                            
+            Console.WriteLine("Art by David Palmer");
+            Console.WriteLine();
         }
     }
 }
